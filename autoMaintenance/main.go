c99ee4e5bddc8848bc8d3b7a89ef24c61053c3fe
jeremy chow <xelloxzxd@gmail.com>
2023-03-18 16:57:23 +0800

    add gRPC Client(Go) & Server(Python), cleanup files

--- autoMaintenance/main.go
+++ autoMaintenance/main.go
@@ -1,40 +1,45 @@
 package main
 
-
 import (
-	"fmt"
+	pb "autoMaintenance/proto"
+	"context"
+
 	"log"
 	"net/http"
 
 	"gopkg.in/robfig/cron.v2"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+func doCheck(c pb.SchedulerClient) {
+	log.Println("doCheck invoked")
+	res, err := c.ScheduleMaintenance(context.Background(), &pb.Equipment{
+		Name: "test",
+	})
+	if err != nil {
+		log.Fatalf("could not check: %v\n", err)
+	}
+
+	log.Printf(res.Status)
 }
 
-func hello(name string) {
-	message := fmt.Sprintf("Hi, %v", name)
-	fmt.Println(message)
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Auto Maintenance Service is running on port 4001."))
 }
 
 func runCronJobs() {
-
 	s := cron.New()
 
-	s.AddFunc("@every 1s", checkMaintenanace)
+	s.AddFunc("@every 1s", checkEquipment)
 
 	s.Start()
     mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on :4001")
+	err := http.ListenAndServe(":4001", mux)
 	log.Fatal(err)
 }
 
-
 func main() {
-    runCronJobs()
-	fmt.Scanln()
+	runCronJobs()
 }
