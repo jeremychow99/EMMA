aaf69e1d36083345806cbfb0c657537d77cd6c63
jeremy chow <xelloxzxd@gmail.com>
2023-03-20 00:41:49 +0800

    rename files

--- autoMaintenance/checkEquipment.go
+++ autoMaintenance/checkEquipment.go
@@ -34,6 +34,20 @@ func doCheck(c pb.SchedulerClient, equipmentID string) {
 	log.Printf(res.Status)
 }
 
+
+type Response struct {
+	Code int `json:"code"`
+	Data struct {
+		Equipment []struct {
+			ID                string `json:"_id"`
+			EquipmentID       string `json:"equipment_id"`
+			EquipmentLocation string `json:"equipment_location"`
+			EquipmentName     string `json:"equipment_name"`
+			LastMaintained    string `json:"last_maintained"`
+		} `json:"equipment"`
+	} `json:"data"`
+}
+
 var addr string = "0.0.0.0:50051"
 
 func checkEquipment() {
